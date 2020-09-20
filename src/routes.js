81d50d630ccc3de436366324cfb11441107e4cb2
Henrique1818 <luiz.henrique01827@gmail.com>
2020-09-19 21:11:06 -0300

    routes

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const ProductController = require('./controllers/ProductController');
+const ProductValidation = require('./middlewares/ProductValidation');
 
 
-router.post('/product', ProductController.create);
+router.get('/products', ProductController.list);
+router.get('/products/item', ProductController.filterName);
+router.get('/products/item/:id', ProductController.show);
+
+router.post('/products', ProductValidation, ProductController.create);
+router.put('/products/item/:id', ProductValidation, ProductController.update);
+router.delete('/products/item/:id', ProductController.delete);
 
 
 module.exports = router;
