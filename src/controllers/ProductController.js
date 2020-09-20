e2563bfe0a8907dc54ce321b7d398ae8df18a79c
Henrique1818 <luiz.henrique01827@gmail.com>
2020-09-19 21:09:57 -0300

    Product Controller

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -7,11 +7,65 @@ class ProductController {
 
         await product.save()
         .then(response => {
-            console.log(response);
             return res.status(200).json(response);
         })
         .catch(error => {
             return res.status(500).json(error);
+        });
+    }
+
+    async list(req, res) {
+        await ProductModel.find()
+        .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(400).json(error);
+        })
+    }
+
+    async update(req, res) {
+        await ProductModel.findByIdAndUpdate({ '_id': req.params.id }, req.body, { new: true })
+        .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(400).json(error);
+        });
+    }
+
+    async show(req, res) {
+        await ProductModel.findById(req.params.id)
+        .then(response => {
+            if(response) {
+                return res.status(200).json(response);
+            } else {
+                return res.status(404).json({ error: 'Product not found'});
+            }
+        })
+        .catch(error => {
+            return res.status(400).json(error);
+        });
+    }
+
+    async filterName(req, res) {
+        await ProductModel.find({'name': {'$eq': req.query.name}})
+        .sort('name')
+        .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(400).json(error);
+        });
+    }
+
+    async delete(req, res) {
+        await ProductModel.deleteOne({ '_id': req.params.id })
+        .then(response => {
+            return res.status(200).json(response)
+        })
+        .catch(error => {
+            return res.status(400).json(error);
         });
     }
 }
